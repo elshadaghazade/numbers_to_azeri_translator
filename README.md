34e5daabeb755d3508c868887b81798bead3372f
user1 <user1@MacBook-Pro-Mac.local>
2021-08-25 01:48:36 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# RƏQƏMLƏRİ AZƏRBAYCAN DİLİNƏ TƏRCÜMƏ EDƏN MODUL
+
+Azərbaycan dilində süni intellekt istifadə etmədən xüsusi alqoritm vasitəsilə tam ədədləri (integer) azərbaycan dilinə çevirən modul.
+
+## İSTİFADƏ QAYDASI
+
+NPM ilə yükləyib istənilən proyektinizdə istifadə edə bilərsiniz. Bunun üçün aşağıdakı kod nümunədən faydalana bilərsiniz:
+
+```sh
+$ npm i numbers_to_azeri_translator
+```
+Daha sonra aşağıdakı qaydada import edərək istifadə edə bilərsiniz:
+
+```js
+const Numbers2Azeri = require('numbers_to_azeri_translator');
+
+const translator = new Numbers2Azeri(3456675430);
+console.log(translator.translate());
+```
+
+Nəticə: ***üç milyard dörd yüz əlli altı milyon altı yüz yetmiş beş min dörd yüz otuz***
---
+++ index.js
@@ -0,0 +1,59 @@
+class NumberAzeriTranslator {
+    teklik = ['', 'bir', 'iki', 'üç', 'dörd', 'beş', 'altı', 'yeddi', 'səkkiz', 'doqquz'];
+    onluq = ['', 'on', 'iyirmi', 'otuz', 'qırx', 'əlli', 'altmış', 'yetmiş', 'səksən', 'doxsan'];
+    yuzluk = ['', 'min', 'milyon', 'milyard', 'trilyon'];
+    constructor (number) {
+        this.number = number;
+    }
+
+    translate () {
+        let divider = 1;
+        const arr = [];
+        let number = this.number;
+    
+        while (divider < number) {
+            const remain = number % (divider * 10);
+            number -= remain;
+            arr.push(remain / (10 ** arr.length));
+            divider *= 10;
+        }
+
+        if (arr.filter(x => x === 0).length === arr.length) {
+            arr.push(1);
+        }
+    
+    
+        let index = 0;
+        let sentense = [];
+        while(arr.length) {
+            const triple = arr.splice(0, 3);
+            const result = this.getTripleString(triple);
+            if (result.length) {
+                sentense.push(this.getTripleString(triple) + ' ' + this.yuzluk[index]);
+            }
+            index++;
+        }
+
+        return sentense.reverse().join(' ');
+    }
+
+    getTripleString(triple) {
+        const arr = [];
+        const [teklik, onluq, yuzluk] = triple;
+        if (yuzluk) {
+            arr.push(`${this.teklik[yuzluk]} yüz`);
+        }
+
+        if (onluq) {
+            arr.push(`${this.onluq[onluq]}`);
+        }
+
+        if (teklik) {
+            arr.push(this.teklik[teklik]);
+        }
+
+        return arr.join(' ');
+    }
+}
+
+module.exports = NumberAzeriTranslator;
